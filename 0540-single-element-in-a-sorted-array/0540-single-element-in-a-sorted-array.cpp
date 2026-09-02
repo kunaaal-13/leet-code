18c947e8f24766fa11681f3c8988aa458f2ee46f
kunaaal-13 <kunaaal1112@gmail.com>
2026-09-02 10:36:58 +0530

    Time: 0 ms (100%), Space: 26 MB (99.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans=ans^nums[i];
+        if(nums.size()==1) return nums[0];
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            int mid= i+(j-i)/2;
+            if(mid==0 && nums[0]!=nums[1]) return nums[0];
+            if(mid==(n-1) && nums[n-1] != nums[n-2]) return nums[n-1];
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            if(mid%2==0){
+                if(nums[mid]==nums[mid-1]){
+                    j=mid-1;
+                }else{
+                    i=mid+1;
+                }
+            }else{
+                if(nums[mid]==nums[mid-1]){
+                    i=mid+1;
+                }else{
+                    j=mid-1;
+                }
+            }
         }
-        return ans;
+        return -1;
     }
 };
