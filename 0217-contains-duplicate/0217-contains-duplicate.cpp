9e9ae03b43fd8a506baef864c8b824b912ee78be
kunaaal-13 <kunaaal1112@gmail.com>
2026-09-05 22:01:25 +0530

    Time: 67 ms (71.2%), Space: 111.4 MB (46.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,7 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> map;
         for(int i=0;i<nums.size();i++){
-            if(map.find(nums[i])!=map.end()){
+            if(map.count(nums[i])>0){
                 return true;
             }
             map[nums[i]]=i;
