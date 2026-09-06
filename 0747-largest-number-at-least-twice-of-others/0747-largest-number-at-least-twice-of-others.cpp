dc3c137b7accb6e19c5913ba3703e20a56c6f676
kunaaal-13 <kunaaal1112@gmail.com>
2026-09-06 12:00:55 +0530

    Time: 0 ms (100%), Space: 13.7 MB (79.76%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int max=nums[0];
+        int index=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>max){
+                max=nums[i];
+                index=i;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(i==index) continue;
+            if(max<nums[i]*2){
+                return -1;
+            }
+        }
+        return index;    
+    }
+};
