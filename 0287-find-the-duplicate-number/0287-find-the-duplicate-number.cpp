7e215f598a4c45d14bafbde2de52bfbab988bc59
kunaaal-13 <kunaaal1112@gmail.com>
2026-09-09 16:07:29 +0530

    Time: 12 ms (41.53%), Space: 65 MB (90.89%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,16 +3,17 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow=nums[0];
         int fast=nums[0];
-        do{
+        while(true){
             slow=nums[slow];
             fast=nums[nums[fast]];
-        }while(slow!=fast);
-        slow=nums[0];
-        while(slow!=fast){
-            slow=nums[slow];
-            fast=nums[fast];
+            if(slow==fast){
+                slow=nums[0];
+                while(slow!=fast){
+                    slow=nums[slow];
+                    fast=nums[fast]; 
+                }
+                return fast;
+            }
         }
-        
-        return fast;
     }
 };
